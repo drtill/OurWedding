2c3ce0f7566c2e2fd5d7d1b283b2716d8d8f3b12
Knot <dr.till007@gmail.com>
2018-01-02 22:30:21 +0700

    KnotAddImage&RotateScript

---
+++ OurWedding/Assets/RotateSelf2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf2 : MonoBehaviour {
+
+    public GameObject imageSprite;
+    // Use this for initialization
+    void Start()
+    {
+        imageSprite.transform.position = new Vector3(25.0f, 0.0f, 0.0f);
+        imageSprite.transform.eulerAngles = new Vector3(0.0f, 0.0f, 120.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 po = imageSprite.transform.position;
+
+        if (po.x >= 4)
+        {
+            po = new Vector3((po.x -= 0.5f), 0);
+            imageSprite.transform.position = po;
+            imageSprite.transform.Rotate(new Vector3(0, 0, 5));
+        }
+
+    }
+}
