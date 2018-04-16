2e58a2252339e67a48bdf9a59f8c6a667b8c63a5
Atthapon Srikosak <dr.till007@gmail.com>
2018-04-16 19:24:44 +0700

    KnotPlayer&TileMap

---
+++ OurWedding/Assets/Presentation/Script/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    Animator anim;
+    SpriteRenderer render;
+    Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		/*if((Input.GetAxisRaw("Horizontal") > 0.5) || (Input.GetAxisRaw("Horizontal") < -0.5))
+        {
+            transform.Translate(new Vector3((Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime), 0.0f, 0.0f));
+        }
+        if ((Input.GetAxisRaw("Vertical") > 0.5) || (Input.GetAxisRaw("Vertical") < -0.5))
+        {
+            transform.Translate(new Vector3(0.0f, (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime), 0.0f));
+        }
+
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            anim.SetBool("moveX", true);
+            anim.SetBool("moveUp", false);
+            anim.SetBool("moveDown", false);
+            render.flipX = true;
+            anim.speed = 1;
+            
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            anim.SetBool("moveX", true);
+            anim.SetBool("moveUp", false);
+            anim.SetBool("moveDown", false);
+            render.flipX = false;
+            anim.speed = 1;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            anim.SetBool("moveX", false);
+            anim.SetBool("moveUp", false);
+            anim.SetBool("moveDown", true);
+            
+            anim.speed = 1;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            anim.SetBool("moveX", false);
+            anim.SetBool("moveUp", true);
+            anim.SetBool("moveDown", false);
+           
+            anim.speed = 1;
+        }
+        else
+        {
+            anim.speed = 0;
+        }*/
+    }
+
+    public void WalkRight()
+    {
+        transform.Translate(new Vector3((0.5f * moveSpeed * Time.deltaTime), 0.0f, 0.0f));
+        anim.SetBool("moveX", true);
+        anim.SetBool("moveUp", false);
+        anim.SetBool("moveDown", false);
+        render.flipX = true;
+        anim.speed = 1;
+    }
+
+    public void WalkLeft()
+    {
+        transform.Translate(new Vector3((-0.5f * moveSpeed * Time.deltaTime), 0.0f, 0.0f));
+        anim.SetBool("moveX", true);
+        anim.SetBool("moveUp", false);
+        anim.SetBool("moveDown", false);
+        render.flipX = false;
+        anim.speed = 1;
+    }
+
+    public void WalkUp()
+    {
+        transform.Translate(new Vector3(0.0f, (0.5f * moveSpeed * Time.deltaTime), 0.0f));
+        anim.SetBool("moveX", false);
+        anim.SetBool("moveUp", true);
+        anim.SetBool("moveDown", false);
+        
+        anim.speed = 1;
+    }
+
+    public void WalkDown()
+    {
+        transform.Translate(new Vector3(0.0f, (-0.5f * moveSpeed * Time.deltaTime), 0.0f));
+        anim.SetBool("moveX", false);
+        anim.SetBool("moveUp", false);
+        anim.SetBool("moveDown", true);
+        anim.speed = 1;
+    }
+}
---
+++ OurWedding/Assets/Presentation/Script/StoryScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryScript : MonoBehaviour {
+
+
+    public int step;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        step = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(step == 0)
+        {
+            player.GetComponent<PlayerController>().WalkLeft();
+
+            if(player.transform.position.x <= -1.5)
+            {
+                step = 1;
+            }
+        }
+        else if(step == 1)
+        {
+            player.GetComponent<PlayerController>().WalkUp();
+
+            if (player.transform.position.y >= 1.5)
+            {
+                step = 2;
+            }
+        }
+        else if (step == 2)
+        {
+            player.GetComponent<PlayerController>().WalkRight();
+
+            if (player.transform.position.x >= 1.5)
+            {
+                step = 3;
+            }
+        }
+
+    }
+}
